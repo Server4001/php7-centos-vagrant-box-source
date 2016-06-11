4c371e1abacbf8bfb4ad3b88c716dba31fb67233
Brice Bentler <server4001@gmail.com>
2016-06-10 22:52:48 -0700

    Adds install of nginx

--- provision/modules/nginx/manifests/init.pp
+++ provision/modules/nginx/manifests/init.pp
@@ -1,5 +1,13 @@
 class nginx {
+    yumrepo { 'nginx':
+        ensure   => 'present',
+        baseurl  => 'http://nginx.org/packages/centos/6/$basearch/',
+        descr    => 'nginx repo',
+        enabled  => '1',
+        gpgcheck => '0',
+    }
     package { "nginx":
         ensure => "installed",
+        require => Yumrepo["nginx"],
     }
 }
