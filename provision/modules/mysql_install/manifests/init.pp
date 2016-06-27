34ba320771e8f812838afe9ee0d82e496dbc4b90
Brice Bentler <server4001@gmail.com>
2016-06-26 23:21:10 -0700

    Adds removal of default MySQL accounts

--- provision/modules/mysql_install/manifests/init.pp
+++ provision/modules/mysql_install/manifests/init.pp
@@ -1,8 +1,9 @@
 class mysql_install {
     class { 'mysql::server':
-        root_password  => "password",
-        package_name   =>  "mysql-community-server",
-        package_ensure => "5.7.13-1.el6",
-        require        => Yumrepo["mysql-connectors-community"],
+        root_password           => "password",
+        remove_default_accounts => true,
+        package_name            =>  "mysql-community-server",
+        package_ensure          => "5.7.13-1.el6",
+        require                 => Yumrepo["mysql-connectors-community"],
     }
 }
