a6ae6a88d7266a1a9dad0d247a170f4f82d93372
Brice Bentler <briceb@julep.com>
2016-12-14 14:00:01 -0800

    Moves mysql root password to hiera file

--- provision/modules/mysql_install/manifests/init.pp
+++ provision/modules/mysql_install/manifests/init.pp
@@ -1,9 +1,11 @@
-class mysql_install {
-    class { 'mysql::server':
-        root_password           => "password",
-        remove_default_accounts => true,
-        package_name            =>  "mysql-community-server",
-        package_ensure          => "5.7.13-1.el6",
-        require                 => Yumrepo["mysql-connectors-community"],
-    }
+class mysql_install (
+  $mysql_root_password
+) {
+  class { 'mysql::server':
+    root_password           => $mysql_root_password,
+    remove_default_accounts => true,
+    package_name            =>  "mysql-community-server",
+    package_ensure          => "5.7.13-1.el6",
+    require                 => Yumrepo["mysql-connectors-community"],
+  }
 }
